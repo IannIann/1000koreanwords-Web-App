9c30cafb694b8c0d2513f2ec98fb7ae4cb2a6d8b
IannIann <fitamant.iann@gmail.com>
2026-04-10 18:33:52 +0200

    Too many attemps error message handled

--- App/1000koreanwords/src/component/Main/Authentification/ForgotPassword.js
+++ App/1000koreanwords/src/component/Main/Authentification/ForgotPassword.js
@@ -3,6 +3,7 @@ import Input from '@app/component/Main/Input';
 import ButtonFlat from '@app/component/Buttons/ButtonFlat';
 import { toast } from 'react-toastify';
 import AuthService from '@app/service/auth.service';
+import tool from '@app/tool/tool';
 import { withRouter } from '@app/tool/withRouter';
 
 import '@app/style/form.css';
@@ -12,6 +13,7 @@ class ForgotPassword extends React.Component {
     state = {
         email: '',
         message: '',
+        errorMessage: '',
         successful: false
     };
 
@@ -32,13 +34,13 @@ class ForgotPassword extends React.Component {
             .then(res => {
                 this.setState({ message: res.message, successful: true });
             })
-            .catch(() => {
-                toast.error('Something went wrong.');
+            .catch((error) => {
+                this.setState({ errorMessage: tool.getErrorMessage(error) });
             });
     }
 
     render() {
-        const { email, message, successful } = this.state;
+        const { email, message, errorMessage, successful } = this.state;
 
         return (
             <div className="component-forgot-password form">
@@ -47,6 +49,8 @@ class ForgotPassword extends React.Component {
                         <div className="form-title small">Forgot your password?</div>
                         <form onSubmit={this.handleSubmit}>
                             <Input id="email" placeholder="Email" type="email" value={email} handler={this.handleChange} />
+                            {errorMessage && <div className="error-message">{errorMessage}</div>}
+                            
                             <ButtonFlat label="Send Reset Link" customClass="button-form" />
                         </form>
                     </>
